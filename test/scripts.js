f167b0feeb9b93e26981ac6532f9d014ac1fc2ad
itachioftheleaf924 <105141523+itachioftheleaf924@users.noreply.github.com>
2022-05-14 12:23:16 -0400

    Add files via upload

---
+++ test/scripts.js
@@ -0,0 +1,19 @@
+function red(){
+    document.getElementById('456').style.fontSize='30px'
+}
+
+function change(){
+    document.querySelector('.mayn').style.fontSize='5px'
+}
+
+function black(){
+    document.querySelector('body').style.backgroundColor='black'
+}
+function white(){
+    document.querySelector('body').style.color='white'
+}
+
+
+function just(){
+document.getElementById("menu").classList.toggle("icon");
+document.getElementById("nav1").classList.toggle("change");}
